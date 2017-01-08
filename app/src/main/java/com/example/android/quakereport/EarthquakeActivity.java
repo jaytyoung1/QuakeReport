0a9877a0f65a1ef874f167815e8a8586a1a91fe1
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2017-01-08 12:36:26 -0500

    Store URL query as constant String

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -29,6 +29,12 @@ public class EarthquakeActivity extends AppCompatActivity
 {
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
 
+    /**
+     * URL for earthquake data from the USGS dataset
+     */
+    private static final String USGS_REQUEST_URL =
+            "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
